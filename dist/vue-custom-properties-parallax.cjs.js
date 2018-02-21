404de023e49a6e851f961004b490d17af194ded9
Vitor Cavalcanti <vitorluizc@outlook.com>
2018-02-21 14:34:42 -0300

    Export directive and install function

---
+++ dist/vue-custom-properties-parallax.cjs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function directive(el, binding, vnode) {
+
+  function setScrollParallax() {
+
+    // document scrolled amount - parallax container offset from top + window height
+    // this make sure to update --scroll-amount only when the elements are in the viewport
+    var scroll = document.documentElement.scrollTop - el.offsetTop + window.innerHeight;
+
+    // set the "--scroll-amount" for each parallax container
+    el.style.setProperty("--scroll-amount", scroll);
+
+    // global "--scroll-amount" attached to the body
+    document.body.style.setProperty("--scroll-amount", document.documentElement.scrollTop);
+
+    window.requestAnimationFrame(setScrollParallax);
+  }
+
+  window.requestAnimationFrame(setScrollParallax);
+}
+
+function install(Vue) {
+  Vue.directive('css-parallax', {
+    bind: directive
+  });
+}
+
+exports.directive = directive;
+exports.install = install;
+exports['default'] = install;
